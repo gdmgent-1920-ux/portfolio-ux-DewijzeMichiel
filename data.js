28593be134b81087eb4cddaa09c8fc8076be58ed
DewijzeMichiel <61418442+DewijzeMichiel@users.noreply.github.com>
2020-03-27 15:48:31 +0100

    Uploaden van assignement 2

--- data.js
+++ data.js
@@ -57,12 +57,12 @@ const data = {
             {
                 "text": "Adobe XD",
                 "title": "Mijn Adobe XD document",
-                "href": "./assignments/assignment2/documents/a2-visual-designs.xd",
+                "href": "./assignments/assignment2/documents/a2-SNEAKERS.BE-visual-designs.xd",
             },
             {
                 "text": "Adobe XD Prototype",
                 "title": "Mijn Adobe XD prototype (share link)",
-                "href": "https://xd.adobe.com/view/e4c35170-a2f7-4c10-753b-ba7c449703ec-92f2/grid",
+                "href": "https://xd.adobe.com/view/67c2e390-d621-4de3-5684-a2b6251a7ea3-11f3/",
                 "target": "_blank"
             }
         ]
